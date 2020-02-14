e938d203afa158d38d719792b670ae2673315c83
Rabie TAWIL <rabietawil@gmail.com>
2020-02-14 13:57:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 
 const companyName = 'Scuber';
 
+<<<<<<< HEAD
 let mostProfitableNeighborhood = 'Chelsea';
 
 let companyCeo = 'Susan Smith';
+=======
+let 
+
+let companyCeo = Susan Smith;
+>>>>>>> 358151b9efeee6883dd1214a1e96b8bcb5ecd674
